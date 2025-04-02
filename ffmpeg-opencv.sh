23195447723b02851aabbb71e9cae7f11f854aa8
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2025-04-02 20:49:40 +0530

    Update ffmpeg-opencv.sh

--- ffmpeg-opencv.sh
+++ ffmpeg-opencv.sh
@@ -21,7 +21,7 @@ sudo apt-get install -y build-essential cmake git pkg-config libgtk-3-dev \
     libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
     libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
     gfortran openexr libatlas-base-dev python3-dev python3-numpy \
-    libtbb2 libtbb-dev libdc1394-dev \
+    libtbb2 libtbb-dev libdc1394-dev \  # Updated package name
     libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
     libavresample-dev libvorbis-dev libxine2-dev \
     libfaac-dev libmp3lame-dev libtheora-dev \
